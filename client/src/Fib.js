a5e47fad0c6174fc556c592a5a1104e1ba9ba876
Azeem Zaidi <azeem.zaidi@gmail.com>
2021-09-19 01:11:11 +1000

    Fixed server/index.js

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -15,7 +15,9 @@ class Fib extends Component {
 
   async fetchValues() {
     const values = await axios.get('/api/values/current');
-    this.setState({ values: values.data });
+    this.setState({ 
+      values: values.data,
+     });
   }
 
   async fetchIndexes() {
--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ app.get('/values/all', async(req, res) => {
 
 app.get('/values/current', async(req, res) => {
   redisClient.hgetall('values', (err, values) => {
-    res.send('values');
+    res.send(values);
   });
 });
 
